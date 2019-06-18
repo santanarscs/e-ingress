25b3e50bc05a519d38f382ef7fc12b14c08fc820
santanarscs <raphaelstn@gmail.com>
2019-06-18 11:35:54 -0300

    configurando store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { GlobalStyle } from './styles/global';
 import { Content, Wrapper, Container } from './styles/components';
+
+import store from './store';
 
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
@@ -10,20 +13,22 @@ import Routes from './routes';
 
 function App() {
   return (
-    <Fragment>
-      <GlobalStyle />
-      <BrowserRouter>
-        <Wrapper>
-          <Container>
-            <Sidebar />
-            <Content>
-              <Header />
-              <Routes />
-            </Content>
-          </Container>
-        </Wrapper>
-      </BrowserRouter>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Wrapper>
+            <Container>
+              <Sidebar />
+              <Content>
+                <Header />
+                <Routes />
+              </Content>
+            </Container>
+          </Wrapper>
+        </BrowserRouter>
+      </Fragment>
+    </Provider>
   );
 }
 
